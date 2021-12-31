ef2df75ab5d30b33e731d86378d234688f26dee8
Débora Oliveira <debora.oliveira@ee.ufcg.edu.br>
2021-12-31 15:58:24 -0300

    changing imutils test identation

--- r3b-soft/cameraimutilsFPSTest.py
+++ r3b-soft/cameraimutilsFPSTest.py
@@ -14,37 +14,36 @@ serverAdressPort = ("192.168.1.104", 8888)
 bufferSize = 1024
 UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 df = pd.DataFrame(columns=['timestamp(microsec)','image'])
-vs = PiVideoStream(resolution=(640,480), framerate=70,).start()
+camera = PiVideoStream(resolution=(640,480), framerate=70,).start()
 
 print("[INFO] setting up camera")
-with vs as camera:
-    # Give the camera some warm-up time
-    time.sleep(2)
+# Give the camera some warm-up time
+time.sleep(2)
     
-    print("[INFO] triggering server")
-    UDPClientSocket.sendto(str.encode("on"),serverAdressPort)
-    msgFromServer = UDPClientSocket.recvfrom(bufferSize)
-    data = msgFromServer[0].split()
-    timeBase = int(data[0])
-    trigger = int(data[1])
-    frameTotal = int(data[2])
-    interval = int(data[3])
-        
-    print("[INFO] waiting camera trigger")
+print("[INFO] triggering server")
+UDPClientSocket.sendto(str.encode("on"),serverAdressPort)
+msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+data = msgFromServer[0].split()
+timeBase = int(data[0])
+trigger = int(data[1])
+frameTotal = int(data[2])
+interval = int(data[3])
+    
+print("[INFO] waiting camera trigger")
+now = time.time_ns()
+while (now - timeBase) < (trigger):
     now = time.time_ns()
-    while (now - timeBase) < (trigger):
+ 
+print('[RECORDING]')
+start = time.time()
+while count < frameTotal:
+    while (now - timeBase) < (interval):
         now = time.time_ns()
-    
-    print('[RECORDING]')
-    start = time.time()
-    while count < frameTotal:
-        while (now - timeBase) < (interval):
-            now = time.time_ns()
-        frame = camera.read()
-        df.loc[len(df.index)] = [now,frame]
-        timeBase += interval
-        count += 1
-    finish = time.time()
+    frame = camera.read()
+    df.loc[len(df.index)] = [now,frame]
+    timeBase += interval
+    count += 1
+finish = time.time()
 
 print('[RESULTS] waited '+ str((now - timeBase)/(10**9)) + 's')
 print('[RESULTS] trigger at timestamp ' +str(now/(10**9)))
--- r4mine-st/cameraimutilsFPSTest.py
+++ r4mine-st/cameraimutilsFPSTest.py
@@ -23,36 +23,35 @@ UDPServerSocket.bind((localIp,localPort))
 print("[INFO] server running...")
 
 df = pd.DataFrame(columns=['timestamp(microsec)','image'])
-vs = PiVideoStream(resolution=(640,480), framerate=70,).start()
+camera = PiVideoStream(resolution=(640,480), framerate=70,).start()
 
 print("[INFO] setting up camera")
-with vs as camera:
-    # Give the camera some warm-up time
-    time.sleep(2)
+# Give the camera some warm-up time
+time.sleep(2)
     
-    print("[INFO] waiting for client")
-    bytesPair = UDPServerSocket.recvfrom(bufferSize)
-    adress = bytesPair[1]
-    timeBase = time.time_ns()
-    print(str(adress) + ' >> '+ str(int(timeBase)/(10**9)))
-    UDPServerSocket.sendto(str.encode(str(timeBase)+' '+str(trigger)+' '+str(frameTotal)+' '+str(interval)),adress)
-    df.loc[len(df.index)] = [timeBase,np.zeros((640,480))]
+print("[INFO] waiting for client")
+bytesPair = UDPServerSocket.recvfrom(bufferSize)
+adress = bytesPair[1]
+timeBase = time.time_ns()
+print(str(adress) + ' >> '+ str(int(timeBase)/(10**9)))
+UDPServerSocket.sendto(str.encode(str(timeBase)+' '+str(trigger)+' '+str(frameTotal)+' '+str(interval)),adress)
+df.loc[len(df.index)] = [timeBase,np.zeros((640,480))]
 
-    print("[INFO] waiting trigger")
+print("[INFO] waiting trigger")
+now = time.time_ns()
+while (now - timeBase) < (trigger):
     now = time.time_ns()
-    while (now - timeBase) < (trigger):
+   
+print('[RECORDING]')
+start = time.time()
+while count < frameTotal:
+    while (now - timeBase) < (interval):
         now = time.time_ns()
-    
-    print('[RECORDING]')
-    start = time.time()
-    while count < frameTotal:
-        while (now - timeBase) < (interval):
-            now = time.time_ns()
-        frame = camera.read()
-        df.loc[len(df.index)] = [now,frame]
-        timeBase += interval
-        count += 1
-    finish = time.time()
+    frame = camera.read()
+    df.loc[len(df.index)] = [now,frame]
+    timeBase += interval
+    count += 1
+finish = time.time()
 
 print('[RESULTS] waited '+ str((now - timeBase)/(10**9)) + 's')
 print('[RESULTS] trigger at timestamp ' +str(now/(10**9)))
