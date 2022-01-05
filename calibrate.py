eb08f4249d8da831ade9b1c0f4bbfead2488655b
Débora Oliveira <debora.oliveira@ee.ufcg.edu.br>
2022-01-05 14:41:32 -0300

    add clib py

---
+++ calibrate.py
@@ -0,0 +1,28 @@
+from time import sleep
+from picamera import PiCamera
+import os
+
+os.system('rm -rf calibPics/*')
+
+camera.resolution = (640, 480)
+camera.framerate = 30
+camera.sensor_mode = 7
+camera.start_preview()
+# allow the camera to warmup
+print('sleeping...')
+time.sleep(5)
+print('awake')
+camera.exposure_mode = 'off'
+g = camera.awb_gains
+camera.awb_mode = 'off'
+camera.awb_gains = g
+i = 0
+
+for filename in camera.capture_continuous('calibPics/img{counter:03d}.jpg'):
+    print('Captured %s' % filename)
+    sleep(2) # wait 5 minutes
+    if i == 10:
+        break
+    i+=1
+    
+os.system('zip -r calib.zip calibPics/')
