52223e82575563eaeee6274e37cb45e313923b61
Debora Oliveira <debora.oliveira@ee.ufcg.edu.br>
2022-02-17 11:29:26 -0300

    correcting struct client code

--- collect/dsCaptureClient.py
+++ collect/dsCaptureClient.py
@@ -1,4 +1,6 @@
-import io,socket
+import io
+import socket
+import struct
 import time
 import picamera
 import os
@@ -35,54 +37,60 @@ bufferSize = 1024
 UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
 client_socket = socket.socket()
-client_socket.connect(('192.168.0.103', 8000))
+client_socket.connect(('192.168.0.103', 8001))
 connection = client_socket.makefile('wb')
 print("[INFO] connected to central...")
 
-with picamera.PiCamera(resolution=(640,480), framerate=40,
+try:
+    with picamera.PiCamera(resolution=(640,480), framerate=40,
                        sensor_mode=4) as camera:
-    camera.start_preview(fullscreen=False,window=(100,100,640,480))
-    print("[INFO] setting up camera")
-    # Give the camera some warm-up time
-    time.sleep(5)
-    camera.shutter_speed = camera.exposure_speed
-    camera.exposure_mode = 'off'
-    camera.color_effects = (128,128)
-    g = camera.awb_gains
-    camera.awb_mode = 'off'
-    camera.awb_gains = g
-    output = SplitFrames(connection)
-    
-    print("[INFO] triggering server")
-    UDPClientSocket.sendto(str.encode("on"),serverAdressPort)
-    msgFromServer = UDPClientSocket.recvfrom(bufferSize)
-    data = msgFromServer[0].split()
-    timeBase = int(data[0])
-    trigger = int(data[1])
-    recTime = int(data[2])
-    
-    os.system('sudo kill -9 '+pid)
-    print('[INFO] killed PTPD process')
+        camera.start_preview(fullscreen=False,window=(100,100,640,480))
+        print("[INFO] setting up camera")
+        time.sleep(5)
+        camera.shutter_speed = camera.exposure_speed
+        camera.exposure_mode = 'off'
+        camera.color_effects = (128,128)
+        g = camera.awb_gains
+        camera.awb_mode = 'off'
+        camera.awb_gains = g
+        output = SplitFrames(connection)
         
-    print("[INFO] waiting camera trigger")
-    now = time.time_ns()
-    while (now - timeBase) < (trigger):
+        print("[INFO] triggering server")
+        UDPClientSocket.sendto(str.encode("on"),serverAdressPort)
+        msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+        data = msgFromServer[0].split()
+        timeBase = int(data[0])
+        trigger = int(data[1])
+        recTime = int(data[2])
+
+        os.system('sudo kill -9 '+pid)
+        print('[INFO] killed PTPD process')
+        
+        print("[INFO] waiting trigger")
         now = time.time_ns()
-    
-    print('[RECORDING..]')
-    start = time.time()
-    camera.start_recording(output, format='mjpeg')
-    camera.wait_recording(recTime)
-    camera.stop_recording()
+        while (now - timeBase) < (trigger):
+            now = time.time_ns()
+        
+        print('[RECORDING..]')
+
+        start = time.time()
+        camera.start_recording(output, format='mjpeg')
+        camera.wait_recording(recTime)
+        camera.stop_recording()
+        finish = time.time()
+        # Write the terminating 0-length to the connection to let the
+        # server know we're done
+        connection.write(struct.pack('<L', 0))
+finally:
+    connection.close()
+    client_socket.close()
     finish = time.time()
-    
-print('Captured %d frames at %.2ffps' % (
-    output.frame_num,
-    output.frame_num / (finish - start)))
-output.df.to_csv('results_1.csv', index = False)
+print('Sent %d images in %d seconds at %.2ffps' % (
+    output.count, finish-start, output.count / (finish-start)))
+output.df.to_csv('results_2.csv', index = False)
 print('[RESULTS] csv exported with '+str(len(output.df.index))+' lines')
 os.system('sudo ptpd -s -i eth0')
 print('[INFO] PTPD running')
-os.system('sshpass -p "debora123#" scp  results_1.csv debora@192.168.0.103:~/Desktop/MoCapRasps/results')
+os.system('sshpass -p "debora123#" scp  results_2.csv debora@192.168.0.103:~/Desktop/MoCapRasps/results')
 print('[INFO] csv sent to central')
-os.system('rm -rf results_1.csv')
+os.system('rm -rf results_2.csv')
