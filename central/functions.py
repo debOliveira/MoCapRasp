0313ebffbd58bbc388dabc229dec6d9b599a4272
Debora Oliveira <debora.oliveira@ee.ufcg.edu.br>
2022-02-28 15:42:14 -0300

    adjust functions to udp server

--- central/functions.py
+++ central/functions.py
@@ -1,98 +1,99 @@
 from cv2.fisheye import undistortPoints
 import numpy as np
 from sklearn import linear_model
-import os,shutil,glob
+from constants import distCoef,cameraMatrix,allCombinationsOf3
 
 def myUndistortPointsFisheye(pts,K,D):
     # save variables
-    pts = pts.reshape(-1,1,2)
-    fx = K[0,0]
-    fy = K[1,1]
-    cx = K[0,2]
-    cy = K[1,2]
+    pts,fx,fy,cx,cy = pts.reshape(-1,1,2),K[0,0],K[1,1],K[0,2],K[1,2]
     # remove destortion
     undPts_norm = undistortPoints(pts, K, D)
     undPts_norm = undPts_norm.reshape(-1,2)
     # remove normalization
     undistPts= np.zeros_like(undPts_norm)
     for i, (x, y) in enumerate(undPts_norm):
-        px = x*fx + cx
-        py = y*fy + cy
-        undistPts[i,0] = px
-        undistPts[i,1] = py    
+        px,py = x*fx + cx,y*fy + cy
+        undistPts[i,0],undistPts[i,1] = px,py
     return undistPts
 
-def getCoordinate(keypoints,idx,cameraMatrix,distCoef):
-    try: 
-        coord = np.zeros((len(idx),2))        
-        k = 0
-        for i in idx:
-            selectedKeypoints = keypoints[int(i)]
-            coord[k] = [selectedKeypoints.pt[0],selectedKeypoints.pt[1]]  
-            k+=1
-    except: 
-        selectedKeypoints = keypoints[idx]
-        coord = [selectedKeypoints.pt[0],selectedKeypoints.pt[1]]  
+def getCoordinate(center,idx,cameraMatrix,distCoef):
+    coord,k = np.zeros((len(idx),2)),0    
+    for i in idx:
+        coord[k] = center[int(i)]  
+        k+=1
     undPt = myUndistortPointsFisheye(np.array(coord), cameraMatrix, distCoef)
-
     return undPt
 
 def isCollinear(p0, p1, p2):
-    X = [[p0[0]], [p1[0]], [p2[0]]]             # X coordinated of all centers
-    y = [p0[1], p1[1], p2[1]]                   # Y coordinated of all centers
-    
+    # coordinated of all centers
+    X,y = [[p0[0]],[p1[0]],[p2[0]]],[p0[1], p1[1], p2[1]] 
     reg = linear_model.LinearRegression()       # create LS model
     reg.fit(X, y)                               # fit model
-    
-    a3 = reg.coef_                              # get line coeficient A
-    b3 = reg.intercept_                         # get line coeficient B
+    a3,b3 = reg.coef_,reg.intercept_            # get line coeficients
 
-    y0_LS = abs(int(a3*p0[0] + b3 - p0[1]))     # find residual between y and 
-    y1_LS = abs(int(a3*p1[0] + b3 - p1[1]))     # the coordinates Y on the LS line
-    y2_LS = abs(int(a3*p2[0] + b3 - p2[1]))
-    
+    y0_LS,y1_LS,y2_LS = abs(int(a3*p0[0] + b3 - p0[1])),abs(int(a3*p1[0] + b3 - p1[1])),abs(int(a3*p2[0] + b3 - p2[1]))     
+    # find residual between y and the coordinates Y on the LS line    
     m = (y0_LS + y1_LS + y2_LS)/3               # get mean value of error
-
     # if it is less than 2 pixel, they are co'llinear
     res = (m < 1)
-
     return res
 
 def reshapeCoord(coord):
     # first reshape array of coordinates
     coordCopy = np.array(coord).reshape(6)
     # get the Y coordinates of each markers' center
-    coordX = [coordCopy[0], coordCopy[2], coordCopy[4]]
-    coordY = [coordCopy[1], coordCopy[3], coordCopy[5]]
+    coordX,coordY = [coordCopy[0], coordCopy[2], coordCopy[4]],[coordCopy[1], coordCopy[3], coordCopy[5]]
     return coordX,coordY
 
 def getOrder(centerX,centerY, baseAxis=False, axis = 1):
     # get wand direction
-    distY = np.array(centerY).max() - np.array(centerY).min()
-    distX = np.array(centerX).max() - np.array(centerX).min()   
+    distY,distX = np.array(centerY).max()-np.array(centerY).min(),np.array(centerX).max()-np.array(centerX).min() 
     # define the order of the markers 
     if not baseAxis:  #if there is no axis to compare, get maximum dist
-        if distY > distX:
-            order = np.argsort(centerY)
-            axis = 1
-        else:
-            order = np.argsort(centerX)
-            axis = 0
+        if distY > distX: order,axis = np.argsort(centerY),1
+        else: order,axis = np.argsort(centerX),0
     else:  # if there is a previous frame, compare to its axis
-        if axis:
-            order = np.argsort(centerY)
-        else:
-            order = np.argsort(centerX)
-
+        if axis: order = np.argsort(centerY)
+        else: order = np.argsort(centerX)
     return order, axis
 
-def cleanFolders(number):
-# clean results folder  
-    try:
-        if os.path.isdir('../results/raw'): 
-            if glob.glob('../results/results_'+str(number)+".csv"):
-                os.system('rm -rf ../results/results_'+str(number)+".csv ../results/*zip")
-            shutil.rmtree('../results/raw/camera'+str(number))
-            os.makedirs('../results/raw/camera'+str(number))
-    except OSError as e:
-        print("Error erasing folder", e.strerror)
+def processCentroids_test(coord,a0,b0):
+    # INIT VARIABLES
+    collinearCentroids,onePassFlag,fourRLinearFlag = [],False,False
+    diameter,centerCoord,doubleCollinearIdx,meanDist = [0,0,0,0],np.zeros((3,2)),np.zeros((2,3)),np.zeros((4,1)) 
+    
+    # get collinear blobs
+    for i in allCombinationsOf3:
+        centerCoord = getCoordinate(coord,i,cameraMatrix,distCoef)  
+        if isCollinear(*centerCoord):
+            if onePassFlag:
+                ourRLinearFlag,doubleCollinearIdx[0],doubleCollinearIdx[1] = True,collinearCentroids,i
+                continue
+            collinearCentroids,onePassFlag = i,True    
+    # if there is ambiguity
+    if fourRLinearFlag: 
+        k=0
+        for i in allCombinationsOf3:
+            centerCoord = getCoordinate(coord,i,cameraMatrix,distCoef)  
+            center = np.mean(centerCoord,axis=0)
+            meanDist[k] = np.mean(np.linalg.norm(centerCoord-center,axis=1))
+            k+=1
+        if allCombinationsOf3[np.argmin(meanDist)] in doubleCollinearIdx:
+            collinearCentroids = allCombinationsOf3[np.argmin(meanDist)]
+    # get fourth marker index
+    is0, = np.where(collinearCentroids == 0)
+    is1, = np.where(collinearCentroids == 1)
+    is2, = np.where(collinearCentroids == 2)
+    if len(is0):
+        if len(is1):
+            if len(is2): markerDPosition = 3
+            else: markerDPosition = 2
+        else: markerDPosition = 1
+    else: markerDPosition = 0
+    # order markers
+    idx = np.hstack((collinearCentroids,markerDPosition))
+    coord = coord[idx]  
+    centerCoord = np.zeros((4,2)) 
+    for i in range(0,4):
+        centerCoord[i] = [coord[i][0]+b0-5,coord[i][1]+a0-5] 
+    return centerCoord
--- central/serverUDP.py
+++ central/serverUDP.py
@@ -1,22 +1,39 @@
 import socket
 import numpy as np
+import warnings
+warnings.filterwarnings("ignore")
+from functions import processCentroids_test
+from cv2 import circle,putText,imshow,waitKey,FONT_HERSHEY_SIMPLEX
 
 bufferSize = 1024
 server_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 server_socket.bind(('0.0.0.0',8888))
 print("[INFO] server running, waiting...")
 
+count,data,verbose = 0,[],True
+
 while True:
     bytesPair = server_socket.recvfrom(bufferSize)
     message = np.frombuffer(bytesPair[0])
-    if not (len(message)-1):
-        break
-    address = bytesPair[1]
+    if not (len(message)-1): break
+    coord,a,b,time = message[0:8].reshape(-1,2),message[8],message[9],message[10]
+    undCoord = processCentroids_test(coord,a,b)
+    count += 1
+    data.append([coord,a,b,undCoord])
 
-    clientMsg = "Message from Client:{}".format(message)
-    clientIP  = "Client IP Address:{}".format(address)
-    
-    print(clientMsg)
-    print(clientIP)
+    if verbose:
+        img,k = np.ones((480,640,3))*25,0
+        for pt in undCoord:
+            center = (int(np.round(pt[0]*16)), int(np.round(pt[1]*16)))
+            circle(img,center,10,(255,0,0),5,shift=4)
+            putText(img,str(k),(int(center[0]/16)-25, int(center[1]/16)-25),FONT_HERSHEY_SIMPLEX,0.5,(255,0,0),2) 
+            k+=1
+        imshow('img',img)
+        waitKey(1)
 
 server_socket.close()
+print('[RESULTS] processed '+str(count)+' images')
+
+# multiple thread server
+# order centroids (needs 2 cameras)
+# triangulate
