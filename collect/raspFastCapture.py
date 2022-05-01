21d855ee56f1005707ca333aeddf0cd59647b5e2
Débora Oliveira <debora.oliveira@ee.ufcg.edu.br>
2022-05-01 20:41:36 -0300

    Add files via upload

---
+++ collect/raspFastCapture.py
@@ -0,0 +1,96 @@
+# Fast reading from the raspberry camera with Python, Numpy, and OpenCV
+# Allows to process grayscale video up to 124 FPS (tested in Raspberry Zero Wifi with V2.1 camera)
+#
+# Made by @CarlosGS in May 2017
+# Club de Robotica - Universidad Autonoma de Madrid
+# http://crm.ii.uam.es/
+# License: Public Domain, attribution appreciated
+
+import numpy as np
+import subprocess as sp
+import time,cv2,atexit,socket
+#import matplotlib.pyplot as plt
+import RPi.GPIO as GPIO
+
+print('[INFO] configuring parameters')
+frames = [] # stores the video sequence for the demo
+N_frames = 0
+# Video capture parameters
+(w,h) = (960,720)
+bytesPerFrame = w * h
+md,ag,dg,co = 4,2,4,100
+fps = 40 # setting to 250 will request the maximum framerate possible
+videoCmd = "./raspividyuv -p 0,0,960,720 -w "+str(w)+" -h "+str(h)+" --output - --timeout 0 --framerate "+str(fps)+" -fli off -cfx 128,128 -ex off --luma -awb off --awbgains 1.3,1.8 -ag "+str(ag)+" -dg "+str(dg)+" -pts - -md "+str(md)+" -co "+str(co)
+videoCmd = videoCmd.split() # Popen requires that each parameter is a separate string
+# turning LED on
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+led = 4
+GPIO.setup(led,GPIO.OUT)
+print("LED on")
+GPIO.output(led,1)
+
+'''print('[INFO] connecting to server')
+# Socket parameters
+UDPSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+UDPSocket.sendto(str('hi').encode(),("192.168.0.104", 8888))
+message,_ = UDPSocket.recvfrom(1024)
+start = float(message.split()[0])
+max_frames = int(message.split()[1])*fps
+print('[INFO] waiting trigger')
+now = time.time()
+while now < start: now = time.time()
+print('[INFO] delay in sec: ',now-start)'''
+max_frames=1000
+
+cameraProcess = sp.Popen(videoCmd, stdout=sp.PIPE) # start the camera
+atexit.register(cameraProcess.terminate) # this closes the camera process in case the python scripts exits unexpectedly
+print("RECORDING ...")
+
+while True:
+    cameraProcess.stdout.flush() # discard any frames that we were not able to process in time
+    # Parse the raw stream into a numpy array
+    frame = np.frombuffer(cameraProcess.stdout.read(bytesPerFrame), dtype=np.uint8)
+    if frame.size != bytesPerFrame:
+        print("Error: Camera stream closed unexpectedly")
+        break
+    frame.shape = (h,w) # set the correct dimensions for the numpy array
+    ts = np.array(cameraProcess.stdout.read(9).decode("utf-8"), dtype="U9")
+    cv2.imwrite('/dev/shm/'+str(ts)+'.bmp',frame)
+    del frame # free the allocated memory
+    N_frames += 1
+    if N_frames == max_frames: break
+    
+end_time = time.time()
+cameraProcess.terminate() # stop the camera
+GPIO.output(led,0)
+
+'''plt.figure(figsize=(4,3))
+plt.plot(np.diff(np.array(frames).astype(float)/1e6),label='Achieved FPS period')
+plt.axhline(y=1/fps,color='r',linestyle='-',label='Reference FPS period')
+plt.xlim(0,np.array(frames).shape[0])
+plt.legend(loc='best')
+plt.grid()
+plt.xlabel('Image number')
+plt.ylabel('Period to previous picture (ms)')
+plt.title('FPS period achieved at '+str(h)+'p, '+str(fps)+'FPS')
+plt.savefig(str(h)+'p'+str(fps)+'FPS.png',dpi=300,bbox_inches="tight")'''
+
+elapsed_seconds = float(ts)/1e6
+print("[RESULTS] "+str(N_frames/elapsed_seconds)+" FPS")
+
+
+'''print("Writing frames to disk...")
+out = cv2.VideoWriter("slow_motion.avi", cv2.cv.CV_FOURCC(*"MJPG"), 30, (w,h))
+for n in range(N_frames):
+    #cv2.imwrite("frame"+str(n)+".png", frames[n]) # save frame as a PNG image
+    frame_rgb = cv2.cvtColor(frames[n],cv2.COLOR_GRAY2RGB) # video codec requires RGB image
+    out.write(frame_rgb)
+out.release()'''
+
+'''print("Display frames with OpenCV...")
+for frame in frames:
+    cv2.imshow("Slow Motion", frame)
+    cv2.waitKey(10) # request maximum refresh rate
+    
+cv2.destroyAllWindows()'''
---
+++ collect/watch.py
@@ -0,0 +1,123 @@
+# import time module, Observer, FileSystemEventHandler
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import cv2,os,socket
+import numpy as np
+
+os.system('rm -rf /dev/shm/*.bmp')
+
+params = cv2.SimpleBlobDetector_Params()
+params.minThreshold = 50   
+params.thresholdStep = 50
+params.maxThreshold = 151
+params.minDistBetweenBlobs = 0
+params.filterByColor = True
+params.filterByArea= True
+params.minArea = 2
+params.filterByConvexity = False
+params.minConvexity = 0.90
+params.blobColor = 0
+params.minRepeatability = 3
+detector = cv2.SimpleBlobDetector_create(params)
+
+pid = os.getpid()
+os.system('sudo renice -n -19 -p '+str(pid))
+times=[]
+frames=[]
+
+# Socket parameters
+UDPSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+def imageProcessing():
+    counter=0          
+    bitsShift = 4
+    constMultiplier = 16      
+    while True:
+        try:
+            start=time.time()
+            img,ts = (yield)
+            _,thresh = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+            coord = cv2.findNonZero(thresh).reshape(-1,2).T
+            xMin,xMax=min(coord[1]),max(coord[1])
+            yMin,yMax=min(coord[0]),max(coord[0])
+            imgWithKPts = cv2.cvtColor(img[xMin-10:xMax+10,yMin-10:yMax+10], cv2.COLOR_GRAY2BGR) 
+            keypoints = detector.detect(cv2.bitwise_not(img[xMin-10:xMax+10,yMin-10:yMax+10]))
+            N = np.array(keypoints).shape[0]
+            print(N)
+            '''msg = np.zeros(N*3+4)
+            for i in range(N): 
+                msg[(i<<1)+i],msg[(i<<1)+i+1],msg[(i<<1)+i+2]=keypoints[i].pt[0],keypoints[i].pt[1],keypoints[i].size
+            msg[-4],msg[-3],msg[-2],msg[-1]= xMin,yMin,ts,counter'''
+            for keyPt in keypoints:
+                center = (int(np.round(keyPt.pt[0]*constMultiplier)), int(np.round(keyPt.pt[1]*constMultiplier)))
+                radius = int(np.round(keyPt.size/2*constMultiplier))
+                imgWithKPts = cv2.circle(imgWithKPts, center, radius, (255,0,0), thickness = 1, lineType = 16, shift = bitsShift)
+                cv2.circle(imgWithKPts, center, 1, (0, 0, 255), -1,  shift = bitsShift)     
+            frames.append(imgWithKPts)    
+            times.append(time.time()-start)
+            counter+=1
+        except GeneratorExit: return
+        except: continue
+          
+class OnMyWatch:
+    # Set the directory on watch
+    watchDirectory = "/dev/shm/"
+  
+    def __init__(self):
+        self.observer = Observer()
+  
+    def run(self):
+        event_handler = Handler()
+        self.observer.schedule(event_handler, self.watchDirectory, recursive = True)
+        self.observer.start()
+        try:
+            while True:
+                time.sleep(120)
+                self.observer.stop()
+                UDPSocket.sendto(np.array([0.0]).tobytes(),("192.168.0.104", 8888))
+                print("Observer Stopped 1")
+                break
+        except:
+            UDPSocket.sendto(np.array([0.0]).tobytes(),("192.168.0.104", 8888))
+            self.observer.stop()
+            print("Observer Stopped 2")
+  
+        self.observer.join()
+  
+  
+class Handler(FileSystemEventHandler):
+    counter = 0  
+    coRout = imageProcessing()
+    coRout.__next__()
+    lastImg = ''
+    
+    @staticmethod
+    def on_any_event(event):
+        if event.is_directory:
+            return None       
+        elif event.event_type == 'created':
+            #print("Watchdog received created event - % s." % event.src_path)
+            if Handler.counter:
+                name = Handler.lastImg
+                img = cv2.imread('/dev/shm/'+name+'.bmp',cv2.IMREAD_GRAYSCALE)
+                if img is not None: Handler.coRout.send((img,int(name)))
+                os.remove('/dev/shm/'+name+'.bmp')
+            Handler.lastImg = event.src_path[-13:-4]
+            Handler.counter+=1
+                    
+                    
+if __name__ == '__main__':
+    watch = OnMyWatch()
+    watch.run()
+    if (len(times)):
+        times = np.array(times[1:])
+        print('[RESULTS] processing with '+str(round(1/np.mean(times),2))+'FPS')
+        print('[RESULTS] '+str(len(times))+' valid images')
+    else: print('[RESULTS] no valid images captured')
+    print("Display frames with OpenCV...")
+    for frame in frames:
+        cv2.imshow("Slow Motion", frame)
+        cv2.waitKey(10) # request maximum refresh rate
+
+    cv2.destroyAllWindows()
