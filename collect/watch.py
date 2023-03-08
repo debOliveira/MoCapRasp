dc3082000542a4e70b7c225a980c570351bc004b
Lorenzo Carrera de Oliveira <loolirer@gmail.com>
2023-03-07 21:59:33 -0300

    Clients find Server via hostname Fixed #1
    Now clients can find Server IP address even if it changes. The hostname set in the code is the one of the Server in the actual arena.

--- collect/watch.py
+++ collect/watch.py
@@ -38,6 +38,7 @@ frames=[]
 
 # Socket parameters
 UDPSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+receiverIP = socket.gethostbyname('debora-pc.local')
 
 def imageProcessing():
     counter=0          
@@ -56,7 +57,7 @@ def imageProcessing():
             for i in range(N): 
                 msg[(i<<1)+i],msg[(i<<1)+i+1],msg[(i<<1)+i+2]=keypoints[i].pt[0],keypoints[i].pt[1],keypoints[i].size
             msg[-4],msg[-3],msg[-2],msg[-1]= xMin,yMin,ts,counter
-            UDPSocket.sendto(msg.tobytes(),("192.168.0.104", 8888))
+            UDPSocket.sendto(msg.tobytes(),(receiverIP, 8888))
             '''imgWithKPts = cv2.cvtColor(img[xMin-10:xMax+10,yMin-10:yMax+10], cv2.COLOR_GRAY2BGR)
             for keyPt in keypoints:
                 center = (int(np.round(keyPt.pt[0]*constMultiplier)), int(np.round(keyPt.pt[1]*constMultiplier)))
@@ -84,11 +85,11 @@ class OnMyWatch:
             while True:
                 time.sleep(300)
                 self.observer.stop()
-                UDPSocket.sendto(np.array([0.0]).tobytes(),("192.168.0.104", 8888))
+                UDPSocket.sendto(np.array([0.0]).tobytes(),(receiverIP, 8888))
                 print("Observer Stopped 1")
                 break
         except:
-            UDPSocket.sendto(np.array([0.0]).tobytes(),("192.168.0.104", 8888))
+            UDPSocket.sendto(np.array([0.0]).tobytes(),(receiverIP, 8888))
             self.observer.stop()
             print("Observer Stopped 2")  
         self.observer.join()
